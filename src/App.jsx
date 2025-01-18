a13307c26ac38e9d29fb54e746e902597b3a8b24
MMhabib <habibleo861@gmail.com>
2025-01-18 12:12:13 +0600

    1st qs

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@
 import './App.css'
 
 function App() {
-  const handleLogin=event=>{
+  const handleregister=event=>{
     event.preventDefault();
     const form=event.target;
     const name=form.name.value;
@@ -31,7 +31,7 @@ fetch('https://react-interview.crd4lc.easypanel.host/api/register',{
   <div className="hero-content flex-col lg:flex-row-reverse">
     
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <form onSubmit={handleLogin} className="card-body">
+      <form onSubmit={handleregister} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Name</span>
@@ -52,7 +52,7 @@ fetch('https://react-interview.crd4lc.easypanel.host/api/register',{
          
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button className="btn btn-primary">Register</button>
         </div>
       </form>
     </div>
